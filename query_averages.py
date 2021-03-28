eecba2e501abf23d0a49133a7fab901de002cb7a
EJEgan <eugene.egan1@ucdconnect.ie>
2021-03-28 15:56:40 +0100

    produces full dataframe with datetime

--- query_averages.py
+++ query_averages.py
@@ -21,21 +21,43 @@ print("Connected")
 cursor = mydb.cursor()
 
 #Pandas read sql query
-sql_select_Query = pd.read_sql_query("SELECT DayOfWeek, StationName, Time, AvailableBikes FROM DublinBikes.LiveHistoricalData", mydb)
+sql_select_Query = pd.read_sql_query("SELECT DayOfWeek, StationNumber, Time, Date, AvailableBikes FROM DublinBikes.LiveHistoricalData", mydb)
 
 #Create a dataframe with all of the rows fetches in the sql query
-df = pd.DataFrame(sql_select_Query, columns=['DayOfWeek', 'StationName', 'Time', 'AvailableBikes'])
+df = pd.DataFrame(sql_select_Query, columns=['DayOfWeek', 'StationNumber', 'Time', 'Date', 'AvailableBikes'])
 
+print(df.head(3))
+# Convert time stored as integer to pandas timedelta
+dt = df['Time']
+df['NumericTime'] =(pd.to_timedelta(dt//100, unit='H') + pd.to_timedelta(dt % 100, unit = 'T'))
+
+# Remove timedelta prefix so that strings can be combined to form date time
+df['NumericTime'] = df['NumericTime'].astype("string")
+df['NumericTime'] = df['NumericTime'].str.slice(start=6, stop=15)
+print("1 \n", df.head(3))
+
+# Recategorize date as a string, combine with time, create datetime column
+df['Date'] = df['Date'].astype("string")
+df['DateTime'] = df['Date'] + df['NumericTime']
+print("2 \n", df.head(3))
+df['DateTime'] = pd.to_datetime(df['DateTime'])
+print("3 \n", df.head(3))
+
+# Select necessary columns to take forward
+df_w_datetime = df[['DayOfWeek', 'StationNumber', 'AvailableBikes', 'DateTime']]
+
+# Save to csv file (smaller than JSON file)
+csv = df_w_datetime.to_csv("bikesDataframe.csv", index = False)
 
 #Group by Station name, day of week, time. (This will be the key for your query to the json object)
-mean_bikes = df.groupby(["StationName","DayOfWeek","Time"]).mean()
+#mean_bikes = df.groupby(["StationNumber","DayOfWeek","Time"]).mean()
 
 #Print out result
 #print(mean_bikes)
 
 
 #Save to Json file (oriented by index)
-result = mean_bikes.to_json(r'mean_bikes.json',orient="index")
+#result = mean_bikes.to_json(r'mean_bikes.json',orient="index")
 
 
 """BELOW IS A SAMPLE QUERY TO THE JSON OBJECT. THIS IS WHAT YOU WILL NEED ON FLASK.
